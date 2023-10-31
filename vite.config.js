fc2dd89970363049ab5720ebc7a2e53d2c766a9c
Andrew Motko <andrew.motko@gmail.com>
2023-10-31 08:31:04 +0200

    vite.config changed

--- vite.config.js
+++ vite.config.js
@@ -17,6 +17,6 @@ export default defineConfig(({ command }) => {
       },
       outDir: '../dist',
     },
-    plugins: [injectHTML(), FullReload(['./src/**/**.html'])],
+    plugins: [injectHTML(), FullReload(['./src/**/**/**.html'])],
   };
 });
